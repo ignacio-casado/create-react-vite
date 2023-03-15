d19b2121e1a436f24afe3fa99e30eb34cf4f1f20
Ignacio casado <nacho.casado01@gmail.com>
2023-03-15 16:58:50 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <nav>
+                <ul className='class-nav'>
+                    <li className='home'>Home</li>
+                    <li className='proyects'>Proyects</li>
+                    <li className='abt-me' >About me</li>
+                    <li className='abt-me'>contact</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default header;
